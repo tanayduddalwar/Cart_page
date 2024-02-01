2bf49f1ab353964af0f68651c4d43f6b39e2e346
Tanay <tanay.duddalwar@gmail.com>
2024-02-01 22:58:36 +0530

    Buttons Animated

--- lib/screens/event_screen.dart
+++ lib/screens/event_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:simple_animated_button/elevated_layer_button.dart';
 import '../widgets/cart_products.dart';
 import '../widgets/event_product.dart';
 
@@ -10,13 +11,36 @@ class EventsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Events Catelog")),
-      body: SafeArea(child: Column(children: [
-        CatelogProducts(),
-        ElevatedButton(onPressed: ()=>Get.to(()=>EventProducts(),duration: Duration(seconds:0)), child: Text("Go to Cart")),
-        
-      ],)),
-      
+      appBar: AppBar(title: Text("Events Catelog"),
+      centerTitle: true,
+      backgroundColor: Colors.lightBlue),
+      body: SafeArea(
+          child: Column(
+        children: [
+          CatelogProducts(),
+          ElevatedLayerButton(
+            onClick: () {
+              Get.to(EventProducts());
+            },
+            buttonHeight: 60,
+            buttonWidth: 270,
+            animationDuration: const Duration(milliseconds: 200),
+            animationCurve: Curves.ease,
+            topDecoration: BoxDecoration(
+              color: Colors.amber,
+              border: Border.all(),
+            ),
+            topLayerChild: Text(
+              "GO TO CART",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            baseDecoration: BoxDecoration(
+              color: Colors.green,
+              border: Border.all(),
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
--- lib/widgets/event_product.dart
+++ lib/widgets/event_product.dart
@@ -1,41 +1,44 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-
-import '../controllers/cart_controller.dart';
+import 'package:animated_icon_button/animated_icon_button.dart';
+import 'package:flutter_animated_button/flutter_animated_button.dart';
 import '../controllers/cart_controller.dart';
 import '../models/event_model.dart';
 
 class CatelogProducts extends StatelessWidget {
-  const CatelogProducts({super.key});
+  const CatelogProducts({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Flexible(
-        child: ListView.builder(
-      itemCount: Event.events.length,
-      itemBuilder: (BuildContext context, int index) {
-        return CatelogProductCard(index: index);
-      },
-    ));
+      child: ListView.builder(
+        itemCount: Event.events.length,
+        itemBuilder: (BuildContext context, int index) {
+          return CatelogProductCard(index: index);
+        },
+      ),
+    );
   }
 }
 
 class CatelogProductCard extends StatelessWidget {
-  final cartController = Get.put(CartController());
+  final CartController cartController = Get.put(CartController());
   final int index;
-  CatelogProductCard({super.key, required this.index});
+
+  CatelogProductCard({Key? key, required this.index}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
           CircleAvatar(
             radius: 20,
             backgroundImage: NetworkImage(Event.events[index].imageUrl),
+            backgroundColor: Colors.lightBlue,
           ),
           SizedBox(width: 20),
           Expanded(child: Text(Event.events[index].name)),
@@ -43,13 +46,31 @@ class CatelogProductCard extends StatelessWidget {
           Expanded(
             child: Text('${Event.events[index].price}'),
           ),
-          IconButton(
-              onPressed: () {
-                cartController.addProduct(Event.events[index]);
-              },
-              icon: Icon(
-                Icons.add_circle,
-              ))
-        ]));
+          
+         
+          AnimatedButton(
+              height: 55,
+              width: 170,
+               animatedOn: AnimatedOn.onHover,
+            onPress: () {
+              cartController.addProduct(Event.events[index]);
+            },
+              text: 'ADD',
+              isReverse: true,
+              selectedTextColor: Colors.white,
+              transitionType: TransitionType.LEFT_BOTTOM_ROUNDER,
+              textStyle: TextStyle(fontWeight: FontWeight.bold),
+              gradient: LinearGradient(colors: [Colors.red, Colors.orange]),
+                selectedGradientColor: LinearGradient(
+                    colors: [Colors.pinkAccent, Colors.purpleAccent]),
+              backgroundColor: Colors.lightBlueAccent,
+              borderColor: Colors.white,
+              selectedBackgroundColor: Colors.lightGreen,
+              borderRadius: 30,
+              borderWidth: 2,
+            ),
+        ],
+      ),
+    );
   }
 }
