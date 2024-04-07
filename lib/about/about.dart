697daed281ad91f811990eadc4c6e5eceff3a7d5
Yuvraj3006 <yuvr3006@gmail.com>
2024-04-07 15:19:04 +0530

    app name and logo changed

--- lib/about/about.dart
+++ lib/about/about.dart
@@ -11,6 +11,7 @@ class PISB extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           leading: IconButton(
